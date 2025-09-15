c6833d959793ba8e0d930c79599376a8dbc4c988
gH7mIDOL <114396373+gH7mIDOL@users.noreply.github.com>
2025-09-15 10:29:59 +0200

    合并了 feature-branch 分支

---
+++ ckmg/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from ckmg.models import *
+# Register your models here.
+
+admin.site.register(news) 
+admin.site.register(subject)
+admin.site.register(result)
+admin.site.register(advertise)
+admin.site.register(alumni)
+admin.site.register(buisness_plan)
+admin.site.register(project)
+admin.site.register(theorynotes)
+admin.site.register(contactus)
+admin.site.register(ppt)
+admin.site.register(modelpaper)
+admin.site.register(slider_image)
+
